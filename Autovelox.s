9a10a2aa5858affa115a16a02efc3452a5e4877b
Francesco Scaburri <74815468+SCABU@users.noreply.github.com>
2024-05-16 11:06:58 +0200

    Update Autovelox.s

--- Autovelox.s
+++ Autovelox.s
@@ -6,10 +6,13 @@ I_O     .half 0                                 # 16 bit volti a rappresentare i
             li $s1, 6500000                     # 55 km/h
             li $s2, 6000000                     # 60 km/h
 
+
+
 inizio:     add $s3 $zero $zero                 #setto la variabile che rappresenta il numero 
             la $s6, IN_OUT                      #salvo l'indirizzo della cella IN_OUT nel registro s6
             li $s5, 268505088                   #(268505088 = 0x10011000)inserisco nel registro $s5, il numero con il 12 bit a 1, la mia mask.
-                                                #cosi facendo settiamo i primi 12 bit a 1 e lasciamo i restanti 20 a 0
+
+						#cosi facendo settiamo i primi 12 bit a 1 e lasciamo i restanti 20 a 0
 
 
 passo1:     lh $s4, 0($s6)                      #controllo con il registro s4 il contenuto(valore) della cella in memoria puntata da t0, il mio IN_OUT
@@ -18,34 +21,37 @@ passo1:     lh $s4, 0($s6)                      #controllo con il registro s4 il
                                             
                                                 #saranno tutte e due a 0  e attivo quindi i primo sensore
             and $t1, $s4, $s5       	        #controllo se la MASK e la cella hanno gli stessi bit corrispondenti, indicando il passaggio della macchina al primo sensore
-            bne $t1, $s5, passo1         
+
+	    bne $t1, $s5, passo1         
 	
             li $s5, 268513280                    #(12288= 0x10013000) setto ad 1 il dodicesimo e il tredicesimo bit della Mask (i bit dei sensori)
 
 # di base faccio lo stesso del primo passo ma la macchina essendo non ancora passata ma in viaggio aggiungo un ciclo per simulare il movimento della macchina
+
+#inizio conteggio istruzioni
 passo2:     lh $s4, 0($s6)                      #stesso controllo di prima anche sul secondo sensore
                                                 #settare adesso il bit a 1 del sensore (senza spegnere il bit del primo sensore!)
                                                 #immaginando che la macchina stia passando 
             and $t1, $s4, $s5         	        #confronto Mask e cella verifico se la macchina è passata dal secondo sensore
 
-            # !!! CAMBIARE DA CICLI A ISTRUZIONI !!!
             addi $s3, $s3, 1        	        #ciclo fino a quando il confronto non si verifica, ad ogni ciclo aumento il contatore di un unità (registro s3).
+	    
             bne $t1, $s5, passo2 
+#fine conteggio istruzioni
+
+            mul $s3, $s3, 4                    # moltiplico il valore di $s3 per 4, ora possiamo confrontare le istruzioni che sono state eseguite tra un sensore ed un altro
 
             slt $t0, $s0, $s3                   #verifico che il numero di cicli (il tempo) sia sia minore o maggiore rispetto a quelli a 50km/h
             bne $t0, $zero, reset  	            #se la velocità è minore, salto a reset, per poi controllare alre macchine al passaggio
 
-
-            #li $t9, 125000000      	        #eseguo un ciclo per passare i 0.5secondi, per evitare di dover eseguire il seguente ciclo troppe volte, 
-loop:       addi $t9, $t9, -1		            #si consiglia di settare manualmente il registro t9.    val=(txfck)/2=125000000
-            bne $t9, $zero, loop 
         
-            # !!! CAMBIARE DA CICLI A ISTRUZIONI !!!
             slt $t0, $s1, $s3                   #se il tempo di passaggio della vettura è maggiore di 55 (velocità compresa tra 50 e 55)	
             beq $t0, $zero, ricerca2    	
+	    
             li $s5, 268513538                   #(268513538=0x10013102)in caso il tempo sia compreso tra 50 e 55, setto l'I_O attivando il bit in posizione 8, e attivamdo il bit in pos 1 della cella, 
             sh $s5, 0($s6) 	                    #indicando lo scatto della fotocamera, salvandola prima in un registro s5 e poi copiando i bit con una sh.
-            j fine                              #salto a fine
+            
+	    j fine                              #salto a fine
 
 
 ricerca2:   slt $t0, $s2, $s3           #verifico che sia compresa tra 55 e 60,
@@ -63,6 +69,7 @@ loop2:      addi $t9, $t9, -1
             bne $t9, $zero, loop2
             
 # resetto se viene superato qualche limite di velocità da una macchina per prepare il programma a un altro giro
+
             li $s5, -16                 #(-16=0x11111100), preparo una maschera per rimettere il bit 1 della cella a 0
             lh $s4, 0($s6)              #leggo e salvo in s4 i bit della cella IN_OUT
             and $t1, $s5, $s4           #rimetto il primo  bit della cella a 0 facendo una "and" tra il valore della mask e quello appena letto e salvato in s4, 
