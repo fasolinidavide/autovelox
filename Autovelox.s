6d3c13bbfb47086ca950dd57e22c3e23f3132186
Fasolini Davide <73524591+fasolinidavide@users.noreply.github.com>
2024-05-17 17:49:45 +0200

    Update Autovelox.s

--- Autovelox.s
+++ Autovelox.s
@@ -2,7 +2,7 @@
 I_O     .half 0                                 # 16 bit volti a rappresentare input/output(impostati a 0)
 
         .text
-            li $s0, 7200000                     # 50 km/h valore da controllare
+            li $s0, 7200000                     # 50 km/h valore da controllare (72 secondi )
             li $s1, 6500000                     # 55 km/h
             li $s2, 6000000                     # 60 km/h
 
@@ -14,24 +14,24 @@ inizio:     add $s3 $zero $zero                 #setto la variabile che rapprese
 	
 		
 
-passo1: lh $s4, 0($s6) 				#per non sporcare il valore di s6 quando andremo ad inserire manualmente il passaggio della macchina, utilizziamo un registro terzo
+passaggio1: lh $s4, 0($s6) 				#per non sporcare il valore di s6 quando andremo ad inserire manualmente il passaggio della macchina, utilizziamo un registro terzo
 
 						#ora settare il valore di $s4 simulando il passaggio della macchina dal primo sensore
 	   
 	and $t1, $s4, $s5 			#confronto mask e valore dei sensori
 
-	bne $t1, $s5, passo1			#cicla fino a quando la macchina non è passata dal primo sensore
+	bne $t1, $s5, passaggio1			#cicla fino a quando la macchina non è passata dal primo sensore
 
 	li $s5, 49152				#settiamo la mask con il 14esimo e 15esimo bit a 1, per controllare il passaggio (setaccio)
 
 
-passo2: lh $s4, 0($s6)                          #per non sporcare il valore di s6 quando andremo ad inserire manualmente il passaggio della macchina, utilizziamo un registro terzo
+passaggio2: lh $s4, 0($s6)                          #per non sporcare il valore di s6 quando andremo ad inserire manualmente il passaggio della macchina, utilizziamo un registro terzo
 						#setta manualmente il bit relativo al secondo settore
 	and $t1, $s4, $s5 			#confronto mask e valore dei sensori
       
         addi $s3, $s3, 1        	        #ciclo fino a quando il confronto non si verifica, ad ogni ciclo aumento il contatore di un unità (registro s3).
 
- 	bne $t1, $s5, passo2 
+ 	bne $t1, $s5, passaggio2 
 
 	#fine conteggio istruzioni
 
@@ -39,13 +39,30 @@ passo2: lh $s4, 0($s6)                          #per non sporcare il valore di s
 
 	slt $t0, $s0, $s3			# controllo se il tempo che impiega la macchina a passare per il secondo sensore 
  		
-  	bne $t0, $zero reset			# con il controllo precedente sappiamo che il tempo percorso è minore di 50km/h(7200000) è minore del tempo impiegato dalla macchina
+  	bne $t0, $zero reset			# con il controllo precedente sappiamo che il tempo percorso è minore di 50km/h(7200000) è minore del tempo impiegato dalla macchina (1100 0000 1000 0000)
 
 
-	slt $t0, $s1, $s3			# controllo se il tempo impiegato dalla macchina è tra 
+	# controllo1
+ 	slt $t0, $s1, $s3			# controllo se il tempo impiegato dalla macchina è maggiore di 50km/h quindi compresa tra 50km/h e 55km/h 
+ 	beq $t0, $zero, controllo2
+
+  	# la macchina ha superato i 50 ma non i 55
+   
+   	
+	li $s5, 129				# velocità compresa tra 50 km/h e 55 km/h -> 0000 0000 1000 0001 (128+1 = 129)
+	sh $s5, 0($s6)				# indicando lo scatto della fotocamera salvandola in s5 e poi in memoria in s6
+ 	j fine
 
 
+controllo2: slt $t0, $s2, $s3			# verifico che sia compreso tra 55 e 60, come ho fatto nel controllo prima
+	    beq $t0, $zero, controllo3		
+     	    li $s5, 49792			# velocità compresa tra 55 km/h e 60 km/h -> 0000 0000 1000 0001 (128+2 = 130)
+	    sh $s5, 0($s6)			# salvo il nono bit a 1 (1100 0010 1000 0000)
+	    j fine
 
+controllo3: li $s5, 50048			#l'unica alternativa è che la velocità sia >60 km/h (1100 0011 1000 0000)
+	    sh $s5, 0($s6)
+  
 reset:      
             sh $zero, 0($s6)              #resetto il registro I_O
             j inizio                    #salto a inizio, controllo la prossima vettura
