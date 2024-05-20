2d49f8daa922401f945d7a2d7082a8cb7658064e
MBertoli14 <168192331+MBertoli14@users.noreply.github.com>
2024-05-20 18:50:20 +0200

    Update Autovelox.s

--- Autovelox.s
+++ Autovelox.s
@@ -18,41 +18,40 @@ passaggio1: lh $s4, 0($s6) 			#per non sporcare il valore di s6 quando andremo a
 
 						#ora settare il valore di $s4 simulando il passaggio della macchina dal primo sensore
 	   
-	and $t1, $s4, $s5 			#confronto mask e valore dei sensori
+	    and $t1, $s4, $s5 			#confronto mask e valore dei sensori
 
-	bne $t1, $s5, passaggio1		#cicla fino a quando la macchina non è passata dal primo sensore
+	    bne $t1, $s5, passaggio1		#cicla fino a quando la macchina non è passata dal primo sensore
 
-	li $s5, 49152				#settiamo la mask con il 14esimo e 15esimo bit a 1, per controllare il passaggio (setaccio)
+	    li $s5, 49152			#settiamo la mask con il 14esimo e 15esimo bit a 1, per controllare il passaggio (setaccio)
 
 
 passaggio2: lh $s4, 0($s6)                      #per non sporcare il valore di s6 quando andremo ad inserire manualmente il passaggio della macchina, utilizziamo un registro terzo
 						#setta manualmente il bit relativo al secondo settore
-	and $t1, $s4, $s5 			#confronto mask e valore dei sensori
+	    and $t1, $s4, $s5 			#confronto mask e valore dei sensori
       
-        addi $s3, $s3, 4        	        #ciclo fino a quando il confronto non si verifica, ad ogni ciclo aumento il contatore di un unità (registro s3).
+            addi $s3, $s3, 4        	        #ciclo fino a quando il confronto non si verifica, ad ogni ciclo aumento il contatore di un unità (registro s3).
 
- 	bne $t1, $s5, passaggio2 
+ 	    bne $t1, $s5, passaggio2 
 
-	#fine conteggio istruzioni
+	    #fine conteggio istruzioni
 
-	slt $t0, $s0, $s3			# controllo se il tempo che impiega la macchina a passare per il secondo sensore 
+	    slt $t0, $s0, $s3			# controllo se il tempo che impiega la macchina a passare per il secondo sensore 
  		
-  	bne $t0, $zero reset			# con il controllo precedente sappiamo che il tempo percorso è minore di 50km/h(7200000) è minore del tempo impiegato dalla macchina (1100 0000 1000 0000)
+  	    bne $t0, $zero reset		# con il controllo precedente sappiamo che il tempo percorso è minore di 50km/h(7200000) è minore del tempo impiegato dalla macchina (1100 0000 1000 0000)
 
-	li $t2, 25000000 			# aspetto un tempo di 0.5s che è il tempo che la macchina fotografica ci mette per fare il reset (VAL=(tx*fck)/2)
- loop:  addi $t2, $t2, -1			
-	bne $t2, $zero, loop			# decremento il tempo di attesa finchè non è arrivato a 0, momento in cui la macchina è di nuovo pronta per fare la foto
+	    li $t2, 25000000 			# aspetto un tempo di 0.5s che è il tempo che la macchina fotografica ci mette per fare il reset (VAL=(tx*fck)/2)
+ loop:      addi $t2, $t2, -1			
+	    bne $t2, $zero, loop		# decremento il tempo di attesa finchè non è arrivato a 0, momento in cui la macchina è di nuovo pronta per fare la foto
 
-	# controllo1
- 	slt $t0, $s1, $s3			# controllo se il tempo impiegato dalla macchina è maggiore di 50km/h quindi compresa tra 50km/h e 55km/h 
- 	beq $t0, $zero, controllo2
+ 	    slt $t0, $s1, $s3			# controllo se il tempo impiegato dalla macchina è maggiore di 50km/h quindi compresa tra 50km/h e 55km/h 
+ 	    beq $t0, $zero, controllo2
 
-  	# la macchina ha superato i 50 ma non i 55
+  	    # la macchina ha superato i 50 ma non i 55
    
    	
-	li $s5, 129				# velocità compresa tra 50 km/h e 55 km/h -> 0000 0000 1000 0001 (128+1 = 129)
-	sh $s5, 0($s6)				# indico lo scatto della fotocamera salvandola in s5 e poi in memoria in s6
- 	j fine
+	    li $s5, 129				# velocità compresa tra 50 km/h e 55 km/h -> 0000 0000 1000 0001 (128+1 = 129)
+	    sh $s5, 0($s6)			# indico lo scatto della fotocamera salvandola in s5 e poi in memoria in s6
+ 	    j fine
 
 
 controllo2: slt $t0, $s2, $s3			# verifico che sia compreso tra 55 e 60, come ho fatto nel controllo prima
